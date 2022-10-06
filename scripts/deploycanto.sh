0e4d2ba12b3462e9808d91dd7e8420b7fd456ec0
Colin Platt <colingplatt@gmail.com>
2022-10-06 17:33:48 +0200

    fixing scripts

--- scripts/deploycanto.sh
+++ scripts/deploycanto.sh
@@ -5,7 +5,7 @@ set -xo errexit
 
 
   npx graph-compiler --config configs/live/canto/nfts.json --include src/datasources --export-schema --export-subgraph
-  npx graph codegen generated/live/nfts.subgraph.yaml
+  npx graph codegen generated/live/canto/nfts.subgraph.yaml
 
   npx graph create --node http://localhost:8020 canto/nfts --access-token $ACCESS_TOKEN nfts.subgraph.yaml
   npx graph deploy --node http://localhost:8020 --ipfs http://localhost:5001 canto/alto --access-token $ACCESS_TOKEN nfts.subgraph.yaml
