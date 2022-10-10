cb0f12a874f731d33b9e9f153f1bfe0ea3f8cfde
Colin Platt <ColinPlatt@users.noreply.github.com>
2022-10-10 15:51:30 +0200

    Update deploycanto.sh

--- scripts/deploycanto.sh
+++ scripts/deploycanto.sh
@@ -4,7 +4,7 @@ set -xo errexit
 
 
 
-  npx graph-compiler --config ~/openzeppelin-subgraph-canto/configs/live/canto/nfts.json --include ~/openzeppelin-subgraph-canto/subgraph/src/datasources --export-schema --export-subgraph
+  npx graph-compiler --config ~/openzeppelin-subgraph-canto/configs/live/canto/nfts.json --include ~/openzeppelin-subgraph-canto/src/datasources --export-schema --export-subgraph
   npx graph codegen ~/openzeppelin-subgraph-canto/generated/live/canto/nfts.subgraph.yaml
 
   npx graph create --node http://localhost:8020 canto/nfts --access-token $ACCESS_TOKEN ~/openzeppelin-subgraph-canto/generated/live/canto/nfts.subgraph.yaml
