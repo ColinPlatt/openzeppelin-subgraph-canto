f32663734dbddc2482adb1507e853b75e6636b60
Colin Platt <ColinPlatt@users.noreply.github.com>
2022-11-09 15:29:24 +0100

    Update deploycanto.sh

--- scripts/deploycanto.sh
+++ scripts/deploycanto.sh
@@ -4,11 +4,11 @@ set -xo errexit
 
 
 
-  npx graph-compiler --config ~/openzeppelin-subgraph-canto/configs/live/canto/nfts.json --include ~/openzeppelin-subgraph-canto/src/datasources --export-schema --export-subgraph
-  npx graph codegen ~/openzeppelin-subgraph-canto/generated/live/canto/nfts.subgraph.yaml
+  npx graph-compiler --config ~/oz-canto/configs/live/canto/nfts.json --include ~/oz-canto/src/datasources --export-schema --export-subgraph
+  npx graph codegen ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
 
-  npx graph create --node http://localhost:8020 canto/nfts --access-token $ACCESS_TOKEN ~/openzeppelin-subgraph-canto/generated/live/canto/nfts.subgraph.yaml
+  npx graph create --node http://localhost:8020 canto/nfts --access-token $ACCESS_TOKEN ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
   sleep 5
-  npx graph deploy --node http://localhost:8020 --ipfs http://localhost:5001 canto/nfts --access-token $ACCESS_TOKEN ~/openzeppelin-subgraph-canto/generated/live/canto/nfts.subgraph.yaml
+  npx graph deploy --node http://localhost:8020 --ipfs http://localhost:5001 canto/nfts --access-token $ACCESS_TOKEN ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
 
 git 
