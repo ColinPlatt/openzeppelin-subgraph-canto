d6096dc82af5b6f998d8438b324aa9915d15d62e
Colin Platt <colingplatt@gmail.com>
2022-11-17 16:27:08 +0100

    changes on refresher tx

--- src/datasources/refresher721.ts
+++ src/datasources/refresher721.ts
@@ -4,6 +4,8 @@ import {
 } from '@graphprotocol/graph-ts'
 
 import {
+	ERC721Contract,
+	ERC721Token,
 	ERC721Transfer,
 } from '../../generated/schema'
 
@@ -13,39 +15,54 @@ import {
 } from '../../generated/refresher721/NFT_Refresher'
 
 import {
-	events,
-	transactions,
-} from '@amxx/graphprotocol-utils'
+	IERC721,
+} from '../../generated/erc721/IERC721'
 
 import {
 	fetchAccount,
 } from '../fetch/account'
 
-import {
-	fetchERC721,
-	fetchERC721Token
-} from '../fetch/erc721'
+function fetchSimpleERC721(contract: Address, identifier: BigInt): ERC721Token {
+	let id = contract.toHex().concat('/').concat(identifier.toHex())
+	let token = ERC721Token.load(id)
+
+	token            = new ERC721Token(id)
+	token.contract   = contract
+	token.identifier = identifier
+	token.approval   = fetchAccount(Address.zero()).id
+
+	let erc721       = IERC721.bind(Address.fromBytes(contract))
+	let try_tokenURI = erc721.try_tokenURI(identifier)
+	token.uri        = try_tokenURI.reverted ? '' : try_tokenURI.value
+
+	return token as ERC721Token
+}
+
 
 export function handleSingle(event: SingleEvent): void {
-	let contract = fetchERC721(event.params.collection)
-	if (contract != null) {
-		let token = fetchERC721Token(contract, event.params.id)
+
+	let token = fetchSimpleERC721(event.params.collection, event.params.id)
+
+	if (token != null) {
 
 		token.save()
 	}
 }
 
 export function handleRange(event: RangeEvent): void {
-	let contract = fetchERC721(event.params.collection)
-	if (contract != null) {
-		let start: BigInt = event.params.idStart;
-		let end: BigInt = event.params.idEnd;
-		let step: BigInt = new BigInt(1);
-		for(let i: BigInt = start; i<=end; i = i.plus(step)) {
 
-			let token = fetchERC721Token(contract, i)
-			token.save()
+	let start: BigInt = event.params.idStart;
+	let end: BigInt = event.params.idEnd;
+	let step: BigInt = new BigInt(1);
 
-		}
+	let tokens: Array<ERC721Token> = new Array<ERC721Token>(end.minus(start).toU32());
+	let i: number = 0;
+
+	for(let id: BigInt = start; id<=end; id = id.plus(step)) {
+
+		tokens[i] = fetchSimpleERC721(event.params.collection, id);
+		tokens[i].save()
+		i++;
+		
 	}
 }
