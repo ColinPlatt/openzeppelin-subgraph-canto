1913ead78031f8a5a7b56d8f9001ed1e781c4232
Colin Platt <colingplatt@gmail.com>
2022-11-21 10:18:20 +0100

    fix script

--- scripts/deploycanto.sh
+++ scripts/deploycanto.sh
@@ -7,8 +7,8 @@ set -xo errexit
   npx graph-compiler --config ~/oz-canto/configs/live/canto/nfts.json --include ~/oz-canto/src/datasources --export-schema --export-subgraph
   npx graph codegen ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
 
-  npx graph create --node http://localhost:8020 canto/nfts --access-token {$ACCESS_TOKEN} ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
+  npx graph create --node http://localhost:8020 canto/nfts_refresh --access-token {$ACCESS_TOKEN} ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
   sleep 5
-  npx graph deploy --node http://localhost:8020 --ipfs http://localhost:5001 canto/nfts --access-token {$ACCESS_TOKEN} ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
+  npx graph deploy --node http://localhost:8020 --ipfs http://localhost:5001 canto/nfts_refresh --access-token {$ACCESS_TOKEN} ~/oz-canto/generated/live/canto/nfts.subgraph.yaml
 
 
